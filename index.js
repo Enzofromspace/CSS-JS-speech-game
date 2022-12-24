65c4a5b4e48fb0c8d892d3d433cfa02c318acdf5
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-24 11:12:49 +0300

    Speech recog works - bug fix results b4 hoohup of color functions

---
+++ index.js
@@ -0,0 +1,21 @@
+import handleResult from './handlers.js';
+
+window.SpeechRecognition = window.SpeechRecogition || window.webkitSpeechRecognition;
+
+function start(){
+//check browswer support
+  if(!('SpeechRecognition' in window)){
+    console.log('browser does not support app');
+    return;
+  }
+  // this runs if browser supports speech recognition
+  console.log('works');
+  // regonize the speech
+  const recognition = new SpeechRecognition();
+  recognition.continuous = true;
+  recognition.interimResults = true;
+  recognition.onresult = handleResult;
+  recognition.start();
+}
+
+start();
